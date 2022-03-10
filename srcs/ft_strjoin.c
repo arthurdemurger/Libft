4b8035d6697b2b0052c1f7539edb5377592b97ef
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-10 18:16:55 +0000

    add ft_strjoin

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -1,27 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/04 14:38:55 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/10 18:16:20 by ademurge         ###   ########.fr       */
+/*   Created: 2022/03/03 18:23:32 by ademurge          #+#    #+#             */
+/*   Updated: 2022/03/10 18:16:11 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
+#include "libft.h"
 
-# define LIBFT_H
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dst;
+	char	*ptr;
+	int		size;
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
-
-size_t			ft_strlen(const char *s);
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb);
-char			*ft_strdup(char *src);
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-
-#endif
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	if (!s1 || !s2)
+		return (NULL);
+	dst = (char *)malloc(sizeof(char) * size);
+	if (!dst)
+		return (NULL);
+	ptr = ft_memcpy(dst, s1, ft_strlen(s1));
+	ft_memcpy(ptr, s2, ft_strlen(s2));
+	return (dst);
+}
