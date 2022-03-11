8126af75f2dad36b9dd5a1533b83e004e928d8f3
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-11 18:35:40 +0100

    add ft_strnstr

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,39 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:10:15 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/03 18:10:16 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/11 17:35:07 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static int	ft_is_equal(char *str, char *to_find, size_t len)
+{
+	int		i;
+
+	i = -1;
+	while (to_find[++i] && len--)
+		if (str[i] != to_find[i])
+			return (0);
+	if (!(++len))
+		return (0);	
+	return (1);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int		i;
+	char	*str;
+	char	*to_find;
+
+	str = (char *)big;
+	to_find = (char *)little;
+	i = -1;
+	if (!*to_find)
+		return (str);
+	while (str[++i] && len--)
+		if (str[i] == *to_find)
+			if (ft_is_equal(&str[i], to_find, len))
+				return (&str[i]);
+	return (0);
+}
