1aedba6950f5ee7112abc2d04b9bb56f6bfe49f5
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-14 13:41:23 +0100

    Add ft_memmove.c

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:09:57 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/10 21:53:03 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/14 12:36:31 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	/*
+	char	*tmp;
+
+	tmp = dst;
 	while (len--)
-	{
-		*(char *)(dst++) = src++;
-	}
-	*/
+		*tmp++ = *(char *)(src++);
+	tmp = 0;
+	return (dst);
 }
