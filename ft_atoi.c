8b8afaf12316e064e358ff85e1c1bf8c9326921b
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-14 17:24:30 +0100

    Modif on ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:09:25 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/04 14:34:36 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/14 16:18:48 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -19,8 +21,6 @@ int	ft_atoi(const char *str)
 	sum = 0;
 	sign = 1;
 	i = 0;
-	if (!str)
-		return (0);
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
@@ -29,7 +29,7 @@ int	ft_atoi(const char *str)
 			sign *= -1;
 		i++;
 	}
-	while (str[i] >= 0 && str[i] <= 9)
-		sum = sum * 10 + str[i] - 48;
+	while (str[i] >= '0' && str[i] <= '9')
+		sum = sum * 10 + str[i++] - 48;
 	return (sum * sign);
 }
