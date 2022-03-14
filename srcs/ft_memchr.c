f88f4bc506a627c49fbc89459b498ec963df0443
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-14 13:19:40 +0100

    Add ft_memchr.c

--- srcs/ft_memchr.c
+++ srcs/ft_memchr.c
@@ -6,7 +6,16 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:09:53 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/03 18:09:54 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/14 12:19:25 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (n--)
+		if (*(char *)(s++) == c)
+			return ((char *)(--s));
+	return (NULL);
+}
