46a00a581c050f6a4a70df9a31daf6beec51c883
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 00:06:44 +0100

    Delete an useless variable

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:10:12 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/14 18:38:53 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/17 23:55:23 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	if (!n)
-		return (0);
 	while (i + 1 < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
