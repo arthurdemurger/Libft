efd62f52ba73e0cc34806fedc565938d73f7b6ac
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 13:19:52 +0100

    Add ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,16 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:43:41 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/14 14:43:42 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 13:15:03 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	(del)(lst->content);
+	free(lst);
+}
