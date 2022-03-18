a02d05f44264bfb50502e569c90d0e9fb36f4c96
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 12:13:21 +0100

    Add ft_lstsize.c

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,23 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:43:50 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/14 14:43:58 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 12:12:42 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int	i;
+
+	i = 0;
+	if (!lst)
+		return (0);
+	while (lst->next)
+	{
+		i++;
+		lst->next = lst->next->next;
+	}
+	return (++i);
+}
