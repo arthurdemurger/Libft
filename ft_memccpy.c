d02d17cc69e245099f6a8e4b4088e491e2e125c3
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 15:55:23 +0100

    Cast into unsigned char

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:09:51 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/18 00:17:46 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 15:55:03 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,12 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	while (n-- && *(char *)(src) != c)
-		*(char *)(dst++) = *(char *)(src++);
-	if (*(char *)(src) == c)
+	while (n-- && *(unsigned char *)(src) != (unsigned char)c)
+		*(unsigned char *)(dst++) = *(char *)(src++);
+	if (*(unsigned char *)(src) == (unsigned char) c)
 	{
-		*(char *)(dst++) = *(char *)(src);
+		*(unsigned char *)(dst++) = *(unsigned char *)(src);
 		return (dst);
 	}
 	return (NULL);
 }
-/*
-int main (void)
-{
-	char	dst_ft[] = "           ewewqeqweq";
-	char	str_ft[] = "aaaaaaaaaaaaaaaaal";
-
-	char	dst_man[] = "          ewewqeqweq";
-	char	str_man[] = "aaaaaaaaaaaaaaaaal";
-	
-	char	c = 'l';
-
-	char	*ret_ft = ft_memccpy(dst_ft, str_ft, c, ft_strlen(str_ft));
-	char	*ret_man = memccpy(dst_man, str_man, c, ft_strlen(str_man));
-	printf("ft : '%s'\nma : '%s'\n\nreturn :\nft : '%s'\
-		nma : '%s'\n", dst_ft, dst_man, ret_ft, ret_man);
-}
-*/
