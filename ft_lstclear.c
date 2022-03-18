8016a32faccb542b1ccb4680eea1fd9104a4f948
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 16:59:05 +0100

    Modif in ft_lstclear.c + ft_strncmp.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:43:39 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/18 13:20:35 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 16:50:16 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*tmp;
+
 	while (lst && del && *lst)
 	{
+		tmp = (*lst)->next;
 		ft_lstdelone(*lst, del);
-		*lst = (*lst)->next;
+		*lst = tmp;
 	}
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 18:10:12 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/17 23:55:23 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 16:56:44 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
+	if (!n)
+		return (0);
 	while (i + 1 < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
