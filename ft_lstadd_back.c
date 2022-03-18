5ed84f085cf64283c415ce6d8bc0fc625b4398c7
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 12:30:43 +0100

    Add ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,16 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:43:32 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/14 14:43:34 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 12:30:07 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
