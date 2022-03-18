773cbd7f901459513c5fa033511ab6b95df4ff24
lirockk <98538729+lirockk@users.noreply.github.com>
2022-03-18 13:20:04 +0100

    ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,17 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:43:39 by ademurge          #+#    #+#             */
-/*   Updated: 2022/03/14 14:43:40 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/03/18 13:19:12 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	while (lst && del && *lst)
+	{
+		ft_lstdelone(*lst, del);
+		*lst = (*lst)->next;
+	}
+}
